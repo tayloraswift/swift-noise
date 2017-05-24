c227555ac2f613e5d5cc923aacad0c396540f355
taylor swift <kelvinsthirteen@gmail.com>
2017-05-24 12:41:39 -0400

    created Seed structs, use more generics to reduce code repetition

--- Sources/Noise/opensimplex.swift
+++ Sources/Noise/opensimplex.swift
@@ -1,109 +1,121 @@
-let simplex_gradient_table_2d:[Double] =
-[
-    -1, -1,     1,  0,    -1,  0,     1,  1,
-    -1,  1,     0, -1,     0,  1,     1, -1
-]
-
-func gradient(u:Int, v:Int, dx:Double, dy:Double) -> Double
+struct Simplex:HashedNoiseGenerator
 {
-    let dr1:Double = 2 - dx*dx - dy*dy
-    if (dr1 > 0)
+    static let n_hashes_2d:Int = 8
+    static let gradient_table_2d:[(Double, Double)] =
+    [
+        (-1, -1),   (1,  0),   (-1,  0),   (1,  1),
+        (-1,  1),   (0, -1),    (0,  1),   (1, -1)
+    ]
+
+    static let radius:Double = 2
+
+    let perm:[Int],
+        perm2d:[Int],
+        amplitude:Double,
+        frequency:Double
+
+    init(amplitude:Double, frequency:Double, seed:Int = 0)
     {
-        let drdr1:Double = dr1 * dr1
-        let hash:Int = random_index_table[(u + random_index_table[v & 255]) & 255] & 14
-        return drdr1 * drdr1 * (simplex_gradient_table_2d[hash] * dx + simplex_gradient_table_2d[hash + 1] * dy)
+        self.amplitude = amplitude
+        self.frequency = frequency
+        (self.perm, self.perm2d) = table(seed: seed, hashes_2d: Simplex.n_hashes_2d)
     }
-    else
+
+    func evaluate(_ x:Double, _ y:Double) -> Double
     {
-        return 0
-    }
-}
+        let x:Double = x * self.frequency,
+            y:Double = y * self.frequency
+        // transform our coordinate system so that the *simplex* (x, y) forms a rectangular grid (u, v)
+        let squish_offset:Double = (x + y) * SQUISH_2D,
+            u:Double = x + squish_offset,
+            v:Double = y + squish_offset
 
-func simplex(_ x:Double, _ y:Double) -> Double
-{
-    // transform our coordinate system so that the *simplex* (x, y) forms a rectangular grid (u, v)
-    let squish_offset:Double = (x + y) * SQUISH_2D,
-        u:Double = x + squish_offset,
-        v:Double = y + squish_offset
+        // get integral (u, v) coordinates of the rhombus
+        let ub:Int = floor(u),
+            vb:Int = floor(v)
 
-    // get integral (u, v) coordinates of the rhombus
-    let ub:Int = floor(u),
-        vb:Int = floor(v)
+        //   (0, 0) ----- (1, 0)
+        //       \    A    / \
+        //         \     /     \                ← (x, y) coordinates
+        //           \ /    B    \
+        //         (0, 1)-------(1, 1)
 
-    //   (0, 0) ----- (1, 0)
-    //       \    A    / \
-    //         \     /     \                ← (x, y) coordinates
-    //           \ /    B    \
-    //         (0, 1)-------(1, 1)
+        //                   (1, -1)
+        //                    /   |
+        //                  /  D  |
+        //                /       |
+        // (ub, vb) = (0, 0) --- (1, 0) -- (2, 0)
+        //          /   |      /  |       /
+        //        /  E  | A  /  B |  C  /       ← (u, v) coordinates
+        //      /       |  /      |   /
+        // (-1, 1) -- (0, 1) --- (1, 1)
+        //              |       /
+        //              |  F  /
+        //              |   /
+        //            (0, 2)
 
-    //                   (1, -1)
-    //                    /   |
-    //                  /  D  |
-    //                /       |
-    // (ub, vb) = (0, 0) --- (1, 0) -- (2, 0)
-    //          /   |      /  |       /
-    //        /  E  | A  /  B |  C  /       ← (u, v) coordinates
-    //      /       |  /      |   /
-    // (-1, 1) -- (0, 1) --- (1, 1)
-    //              |       /
-    //              |  F  /
-    //              |   /
-    //            (0, 2)
+        // get relative position inside the rhombus relative to (ub, vb)
+        let du0:Double = u - Double(ub),
+            dv0:Double = v - Double(vb)
 
-    // get relative position inside the rhombus relative to (ub, vb)
-    let du0:Double = u - Double(ub),
-        dv0:Double = v - Double(vb)
+        // do the same in the original (x, y) coordinate space
 
-    // do the same in the original (x, y) coordinate space
+        // stretch back to get (x, y) coordinates of rhombus origin
+        let stretch_offset:Double = Double(ub + vb) * STRETCH_2D,
+            xb:Double = Double(ub) + stretch_offset,
+            yb:Double = Double(vb) + stretch_offset
 
-    // stretch back to get (x, y) coordinates of rhombus origin
-    let stretch_offset:Double = Double(ub + vb) * STRETCH_2D,
-        xb:Double = Double(ub) + stretch_offset,
-        yb:Double = Double(vb) + stretch_offset
+        // get relative position inside the rhombus relative to (xb, xb)
+        let dx0:Double = x - xb,
+            dy0:Double = y - yb
 
-    // get relative position inside the rhombus relative to (xb, xb)
-    let dx0:Double = x - xb,
-        dy0:Double = y - yb
+        var z:Double = 0 // the value of the noise function, which we will sum up
 
-    var z:Double = 0 // the value of the noise function, which we will sum up
+        // contribution from (1, 0)
+        z += gradient(u : ub + 1,
+                      v : vb,
+                      dx: dx0 - 1 - STRETCH_2D,
+                      dy: dy0     - STRETCH_2D)
 
-    // contribution from (1, 0)
-    z += gradient(u : ub + 1,
-                  v : vb,
-                  dx: dx0 - 1 - STRETCH_2D,
-                  dy: dy0     - STRETCH_2D)
+        // contribution from (0, 1)
+        z += gradient(u : ub,
+                      v : vb + 1,
+                      dx: dx0     - STRETCH_2D,
+                      dy: dy0 - 1 - STRETCH_2D)
 
-    // contribution from (0, 1)
-    z += gradient(u : ub,
-                  v : vb + 1,
-                  dx: dx0     - STRETCH_2D,
-                  dy: dy0 - 1 - STRETCH_2D)
-
-    // decide which triangle we are in
-    let uv_sum:Double = du0 + dv0
-    if (uv_sum > 1) // we are to the bottom-right of the diagonal line (du = 1 - dv)
-    {
-        z += gradient(u : ub  + 1,
-                      v : vb  + 1,
-                      dx: dx0 - 1 - 2*STRETCH_2D,
-                      dy: dy0 - 1 - 2*STRETCH_2D)
-
-        let center_dist:Double = 2 - uv_sum
-        if center_dist < du0 || center_dist < dv0
+        // decide which triangle we are in
+        let uv_sum:Double = du0 + dv0
+        if (uv_sum > 1) // we are to the bottom-right of the diagonal line (du = 1 - dv)
         {
-            if du0 > dv0
+            z += gradient(u : ub  + 1,
+                          v : vb  + 1,
+                          dx: dx0 - 1 - 2*STRETCH_2D,
+                          dy: dy0 - 1 - 2*STRETCH_2D)
+
+            let center_dist:Double = 2 - uv_sum
+            if center_dist < du0 || center_dist < dv0
             {
-                z += gradient(u : ub  + 2,
-                              v : vb     ,
-                              dx: dx0 - 2 - 2*STRETCH_2D,
-                              dy: dy0     - 2*STRETCH_2D)
+                if du0 > dv0
+                {
+                    z += gradient(u : ub  + 2,
+                                  v : vb     ,
+                                  dx: dx0 - 2 - 2*STRETCH_2D,
+                                  dy: dy0     - 2*STRETCH_2D)
+                }
+                else
+                {
+                    z += gradient(u : ub     ,
+                                  v : vb  + 2,
+                                  dx: dx0     - 2*STRETCH_2D,
+                                  dy: dy0 - 2 - 2*STRETCH_2D)
+                }
             }
             else
             {
-                z += gradient(u : ub     ,
-                              v : vb  + 2,
-                              dx: dx0     - 2*STRETCH_2D,
-                              dy: dy0 - 2 - 2*STRETCH_2D)
+                z += gradient(u : ub,
+                              v : vb,
+                              dx: dx0,
+                              dy: dy0)
             }
         }
         else
@@ -112,41 +124,34 @@ func simplex(_ x:Double, _ y:Double) -> Double
                           v : vb,
                           dx: dx0,
                           dy: dy0)
-        }
-    }
-    else
-    {
-        z += gradient(u : ub,
-                      v : vb,
-                      dx: dx0,
-                      dy: dy0)
 
-        let center_dist:Double = 1 - uv_sum
-        if center_dist > du0 || center_dist > dv0
-        {
-            if du0 > dv0
+            let center_dist:Double = 1 - uv_sum
+            if center_dist > du0 || center_dist > dv0
             {
-                z += gradient(u : ub  + 1,
-                              v : vb  - 1,
-                              dx: dx0 + 1,
-                              dy: dy0 - 1)
+                if du0 > dv0
+                {
+                    z += gradient(u : ub  + 1,
+                                  v : vb  - 1,
+                                  dx: dx0 + 1,
+                                  dy: dy0 - 1)
+                }
+                else
+                {
+                    z += gradient(u : ub  - 1,
+                                  v : vb  + 1,
+                                  dx: dx0 - 1,
+                                  dy: dy0 + 1)
+                }
             }
             else
             {
-                z += gradient(u : ub  - 1,
+                z += gradient(u : ub  + 1,
                               v : vb  + 1,
-                              dx: dx0 - 1,
-                              dy: dy0 + 1)
+                              dx: dx0 - 1 - 2*STRETCH_2D,
+                              dy: dy0 - 1 - 2*STRETCH_2D)
             }
         }
-        else
-        {
-            z += gradient(u : ub  + 1,
-                          v : vb  + 1,
-                          dx: dx0 - 1 - 2*STRETCH_2D,
-                          dy: dy0 - 1 - 2*STRETCH_2D)
-        }
-    }
 
-    return z * 1/14
+        return self.amplitude * z
+    }
 }
--- Sources/Noise/supersimplex.swift
+++ Sources/Noise/supersimplex.swift
@@ -1,223 +1,197 @@
 import func Glibc.sin
 import func Glibc.cos
 
-let N_GRADIENTS:Int = 256
-
-let super_gradient_table_2d:[(Double, Double)] = (0 ..< N_GRADIENTS).lazy.map
-{ Double($0) * 2 * Double.pi/Double(N_GRADIENTS) }.map
+struct SuperSimplex:HashedNoiseGenerator
 {
-    let x:Double = 10*cos($0),
-        y:Double = 10*sin($0)
-    return (x, y)
-}
-
-func random_table(seed:Int) -> ([Int], [Int])
-{
-    var seed = seed
-    var perm:[Int]   = [Int](repeating: 0, count: 1024),
-        perm2D:[Int] = [Int](repeating: 0, count: 1024)
-    var source:[Int] = Array(0 ..< 1024)
-    for i in stride(from: 1023, to: 0, by: -1)
+    struct LatticePoint2D
     {
-        seed = seed &* 6364136223846793005 &+ 1442695040888963407
-        var r:Int = (seed + 31) % (i + 1)
-        if r < 0
+        let u:Int,
+            v:Int,
+            dx:Double,
+            dy:Double
+
+        init(u:Int, v:Int)
         {
-            r += i + 1
+            let stretch_offset:Double = Double(u + v) * SQUISH_2D
+            self.u = u
+            self.v = v
+            self.dx = Double(u) + stretch_offset
+            self.dy = Double(v) + stretch_offset
         }
-        perm[i]   = source[r]
-        perm2D[i] = perm[i] % N_GRADIENTS
-        //perm3D[i] = (short)((perm[i] % 48) * 3);
-        source[r] = source[i]
     }
 
-    return (perm, perm2D)
-}
-
-let (perm, perm2D):([Int], [Int]) = random_table(seed: 0)
-
-var histogram:[Int] = [Int](repeating: 0, count: N_GRADIENTS)
-
-func supergradient(u:Int, v:Int, dx:Double, dy:Double) -> Double
-{
-    let dr:Double = 2/3 - dx*dx - dy*dy // why 2/3 ?
-    if (dr > 0)
+    static let n_hashes_2d:Int = 16
+    static let gradient_table_2d:[(Double, Double)] = (0 ..< SuperSimplex.n_hashes_2d).lazy.map
+    { Double($0) * 2 * Double.pi/Double(SuperSimplex.n_hashes_2d) }.map
     {
-        let drdr:Double = dr * dr
-        let hash:Int = perm2D[perm[u & 1023] ^ (v & 1023)],
-            gradient:(Double, Double) = super_gradient_table_2d[hash]
-        histogram[hash] += 1
-        return drdr * drdr * (gradient.0 * dx + gradient.1 * dy)
+        let x:Double = 10*cos($0),
+            y:Double = 10*sin($0)
+        return (x, y)
     }
-    else
+
+    static let points_2d:[LatticePoint2D] =
+    [
+        ((-1,  0), ( 0, -1)),
+        (( 0,  1), ( 1,  0)),
+        (( 1,  0), ( 0, -1)),
+        (( 2,  1), ( 1,  0)),
+        ((-1,  0), ( 0,  1)),
+        (( 0,  1), ( 1,  2)),
+        (( 1,  0), ( 0,  1)),
+        (( 2,  1), ( 1,  2)),
+    ].map
     {
-        return 0
-    }
-}
+        return [LatticePoint2D(u:    0, v:    0),
+                LatticePoint2D(u:    1, v:    1),
+                LatticePoint2D(u: $0.0, v: $0.1),
+                LatticePoint2D(u: $1.0, v: $1.1)]
+    }.flatMap{ $0 }
 
-struct LatticePoint2D
-{
-    let u:Int,
-        v:Int,
-        dx:Double,
-        dy:Double
+    static let radius:Double = 2/3
 
-    init(u:Int, v:Int)
+    let perm:[Int],
+        perm2d:[Int],
+        amplitude:Double,
+        frequency:Double
+
+    init(amplitude:Double, frequency:Double, seed:Int = 0)
     {
-        let stretch_offset:Double = Double(u + v) * SQUISH_2D
-        self.u = u
-        self.v = v
-        self.dx = Double(u) + stretch_offset
-        self.dy = Double(v) + stretch_offset
+        self.amplitude = amplitude
+        self.frequency = frequency
+        (self.perm, self.perm2d) = table(seed: seed, hashes_2d: SuperSimplex.n_hashes_2d)
     }
-}
 
-let SS_LOOKUP_2D:[LatticePoint2D] =
-[
-    ((-1,  0), ( 0, -1)),
-    (( 0,  1), ( 1,  0)),
-    (( 1,  0), ( 0, -1)),
-    (( 2,  1), ( 1,  0)),
-    ((-1,  0), ( 0,  1)),
-    (( 0,  1), ( 1,  2)),
-    (( 1,  0), ( 0,  1)),
-    (( 2,  1), ( 1,  2)),
-].map
-{
-    return [LatticePoint2D(u:    0, v:    0),
-            LatticePoint2D(u:    1, v:    1),
-            LatticePoint2D(u: $0.0, v: $0.1),
-            LatticePoint2D(u: $1.0, v: $1.1)]
-}.flatMap{ $0 }
-
-func super_simplex(_ x:Double, _ y:Double) -> Double
-{
-    // transform our (x, y) coordinate to (u, v) space
-    let stretch_offset:Double = (x + y) * STRETCH_2D,
-        u:Double = x + stretch_offset,
-        v:Double = y + stretch_offset
-
-    //         (0, 0) ----- (1, 0)
-    //           / \    A    /
-    //         /     \     /                ← (x, y) coordinates
-    //       /    B    \ /
-    //    (0, 1) ----- (1, 1)
-
-    //             (-1, 0)
-    //                | \
-    //                |    \
-    //                |   C   \
-    //  (0, -1) -- (0, 0) -- (1, 0)
-    //       \   D   | \   A   | \
-    //          \    |    \    |    \       ← (u, v) coordinates
-    //             \ |   B   \ |   E   \
-    //            (0, 1) -- (1, 1) -- (2, 1)
-    //                  \   F   |
-    //                     \    |
-    //                        \ |
-    //                       (1, 2)
-
-    // use the (u, v) coordinates to bin the triangle
-    let ub:Int = floor(u),
-        vb:Int = floor(v)
-
-    // get relative offsets from the top-left corner of the square (in (u, v) space)
-    let du0:Double = u - Double(ub),
-        dv0:Double = v - Double(vb)
-
-    let a:Int = du0 + dv0 > 1 ? 1 : 0
-    let vertex_index:Int = a << 2 |
-        Int((2*du0 - dv0 - Double(a))*0.5 + 1) << 3 |
-        Int((2*dv0 - du0 - Double(a))*0.5 + 1) << 4
-    /*
-        This bit of code deserves some explanation. OpenSimplex/SuperSimplex
-        always samples four vertices. Which four depends on what part of the A–B
-        square our (x, y) → (u, v) sample coordinate lands in. Think of each
-        triangular slice of that square as being further subdivided into three
-        smaller triangles.
-
-        ************************
-        **  *           a     **
-        *  * *     *       *   *
-        *   *   *      *       *
-        *    *    *   b   *  c *
-        *     *   e  *     *   *
-        *  d   *       *    *  *
-        *    *      *    *   * *
-        *  *            *  *  **
-        **        f           **
-        ************************
-
-        Obviously we’re running up against the bounds on what can be explained
-        with an ASCII art comment. Hopefully you get the idea. We have 6 regions,
-        counter-clockwise in the upper-right triangle from the top, regions a, b,
-        and c, and clockwise in the lower-left triangle from the left, regions
-        d, e, and f.
-
-        Region a borders region C, so it gets the extra vertices (-1, 0) and (1, 0)
-        in addition to (0, 0), and (1, 1), which every region samples. Region c
-        borders region E, so it gets the extra vertices (1, 0) and (2, 1). Region b
-        doesn’t border any exterior region, so it just gets the other two vertices
-        of the square, (0, 1) and (1, 0). Regions d and f are the same as a and c,
-        except they border D and F, respectively. Region e is essentially the same
-        as region b.
-
-        This means that we effectively have five different vertex selection outcomes.
-        That means we need no less than three bits of information, i.e., three tests
-        to determine where we are. Two such tests could be:
-
-        (0, 0) -------------- (1, 0)
-            | \        \       |
-            |  \        \      |
-            |   \        \     |
-            |    \        \    |
-            |     \        \   |
-            |      \        \  |
-            |       \        \ |
-        (1, 0) -------------- (1, 1)
-                   v = 2u    v = 2u - 1
-
-        and
-
-        (0, 0) -------------- (1, 0)
-            |   -              |
-            |       -          |
-            |           -      |
-            |               -  |
-u = 2v - 1  |   -              |  u = 2v
-            |       -          |
-            |           -      |
-        (1, 0) -------------- (1, 1)
-
-        Each test has two lines. How do we pick which one? We use a third test:
-
-        (0, 0) -------------- (1, 0)
-            |              /   |
-            |            /     |
-            |          /       |
-            |        /         |
-            |      /           |
-            |    /  u + v = 1  |
-            |  /               |
-        (1, 0) -------------- (1, 1)
-
-        If we’re in the top left, we use the lines without the offsets, otherwise
-        we use the ones with the -1 offset. That’s where the `- Double(a)` term
-        comes from.
-    */
-
-    // get the relative offset from (0, 0)
-    let squish_offset:Double = (du0 + dv0) * SQUISH_2D,
-        dx0:Double = du0 + squish_offset,
-        dy0:Double = dv0 + squish_offset
-
-    var z:Double = 0
-    for point in SS_LOOKUP_2D[vertex_index ..< vertex_index + 4]
+    func evaluate(_ x:Double, _ y:Double) -> Double
     {
-        // get the relative offset from *that* particular point
-        let dx:Double = dx0 - point.dx,
-            dy:Double = dy0 - point.dy
-        z += supergradient(u: ub + point.u, v: vb + point.v, dx: dx, dy: dy)
+        let x:Double = x * self.frequency,
+            y:Double = y * self.frequency
+        // transform our (x, y) coordinate to (u, v) space
+        let stretch_offset:Double = (x + y) * STRETCH_2D,
+            u:Double = x + stretch_offset,
+            v:Double = y + stretch_offset
+
+        //         (0, 0) ----- (1, 0)
+        //           / \    A    /
+        //         /     \     /                ← (x, y) coordinates
+        //       /    B    \ /
+        //    (0, 1) ----- (1, 1)
+
+        //             (-1, 0)
+        //                | \
+        //                |    \
+        //                |   C   \
+        //  (0, -1) -- (0, 0) -- (1, 0)
+        //       \   D   | \   A   | \
+        //          \    |    \    |    \       ← (u, v) coordinates
+        //             \ |   B   \ |   E   \
+        //            (0, 1) -- (1, 1) -- (2, 1)
+        //                  \   F   |
+        //                     \    |
+        //                        \ |
+        //                       (1, 2)
+
+        // use the (u, v) coordinates to bin the triangle
+        let ub:Int = floor(u),
+            vb:Int = floor(v)
+
+        // get relative offsets from the top-left corner of the square (in (u, v) space)
+        let du0:Double = u - Double(ub),
+            dv0:Double = v - Double(vb)
+
+        let a:Int = du0 + dv0 > 1 ? 1 : 0
+        let vertex_index:Int = a << 2 |
+            Int((2*du0 - dv0 - Double(a))*0.5 + 1) << 3 |
+            Int((2*dv0 - du0 - Double(a))*0.5 + 1) << 4
+        /*
+            This bit of code deserves some explanation. OpenSimplex/SuperSimplex
+            always samples four vertices. Which four depends on what part of the A–B
+            square our (x, y) → (u, v) sample coordinate lands in. Think of each
+            triangular slice of that square as being further subdivided into three
+            smaller triangles.
+
+            ************************
+            **  *           a     **
+            *  * *     *       *   *
+            *   *   *      *       *
+            *    *    *   b   *  c *
+            *     *   e  *     *   *
+            *  d   *       *    *  *
+            *    *      *    *   * *
+            *  *            *  *  **
+            **        f           **
+            ************************
+
+            Obviously we’re running up against the bounds on what can be explained
+            with an ASCII art comment. Hopefully you get the idea. We have 6 regions,
+            counter-clockwise in the upper-right triangle from the top, regions a, b,
+            and c, and clockwise in the lower-left triangle from the left, regions
+            d, e, and f.
+
+            Region a borders region C, so it gets the extra vertices (-1, 0) and (1, 0)
+            in addition to (0, 0), and (1, 1), which every region samples. Region c
+            borders region E, so it gets the extra vertices (1, 0) and (2, 1). Region b
+            doesn’t border any exterior region, so it just gets the other two vertices
+            of the square, (0, 1) and (1, 0). Regions d and f are the same as a and c,
+            except they border D and F, respectively. Region e is essentially the same
+            as region b.
+
+            This means that we effectively have five different vertex selection outcomes.
+            That means we need no less than three bits of information, i.e., three tests
+            to determine where we are. Two such tests could be:
+
+            (0, 0) -------------- (1, 0)
+                | \        \       |
+                |  \        \      |
+                |   \        \     |
+                |    \        \    |
+                |     \        \   |
+                |      \        \  |
+                |       \        \ |
+            (1, 0) -------------- (1, 1)
+                       v = 2u    v = 2u - 1
+
+            and
+
+            (0, 0) -------------- (1, 0)
+                |   -              |
+                |       -          |
+                |           -      |
+                |               -  |
+    u = 2v - 1  |   -              |  u = 2v
+                |       -          |
+                |           -      |
+            (1, 0) -------------- (1, 1)
+
+            Each test has two lines. How do we pick which one? We use a third test:
+
+            (0, 0) -------------- (1, 0)
+                |              /   |
+                |            /     |
+                |          /       |
+                |        /         |
+                |      /           |
+                |    /  u + v = 1  |
+                |  /               |
+            (1, 0) -------------- (1, 1)
+
+            If we’re in the top left, we use the lines without the offsets, otherwise
+            we use the ones with the -1 offset. That’s where the `- Double(a)` term
+            comes from.
+        */
+
+        // get the relative offset from (0, 0)
+        let squish_offset:Double = (du0 + dv0) * SQUISH_2D,
+            dx0:Double = du0 + squish_offset,
+            dy0:Double = dv0 + squish_offset
+
+        var z:Double = 0
+        for point in SuperSimplex.points_2d[vertex_index ..< vertex_index + 4]
+        {
+            // get the relative offset from *that* particular point
+            let dx:Double = dx0 - point.dx,
+                dy:Double = dy0 - point.dy
+            z += self.gradient(u: ub + point.u, v: vb + point.v, dx: dx, dy: dy)
+        }
+        return self.amplitude * z
     }
-    return z
 }
