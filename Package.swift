3965dcd7755d43d548ba4ade9b9c3ff5eb8b158e
Joe Heck <heckj@mac.com>
2024-05-30 15:42:24 -0700

    update dependencies, swift version, and .gitignore - latest swift-png, swift 5.8, and add platform lower-bound constaints for compilation

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
     name: "swift-noise",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: 
     [
         .library(name: "Noise", targets: ["Noise"]),
@@ -11,18 +12,23 @@ let package = Package(
     ],
     dependencies: 
     [
-        .package(url: "https://github.com/kelvin13/swift-png", .upToNextMinor(from: "4.0.1"))
+        .package(url: "https://github.com/tayloraswift/swift-png", from: "4.4.0")
     ],
     targets: 
     [
-        .target(name: "Noise"),
+        .target(
+            name: "Noise"
+        ),
 
-        .executableTarget(name: "NoiseTests", 
-            dependencies: 
-            [
-                .target(name: "Noise"), 
-                .product(name: "PNG", package: "swift-png"), 
-            ],
-            path: "Tests/NoiseTests")
+        .executableTarget(name: "NoiseTests",
+                          dependencies: [
+                            .target(name: "Noise"),
+                            .product(name: "PNG", package: "swift-png"),
+                          ],
+                          path: "Tests/NoiseTests",
+                          exclude:[
+                            "calibrate.blend",
+                            "calibrate.blend1"
+                          ])
     ]
 )
